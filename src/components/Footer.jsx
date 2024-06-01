b544d0a5d9921ebfc7da74054cb492a23e7a483b
bhanukumar <bhanu.k@cubiclogics.in>
2024-06-01 12:44:11 +0530

    header and footer created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import { Container,Row,Col } from "react-bootstrap"
+
+const Footer=()=>{
+    const year=new Date().getFullYear()
+    return <footer>
+        <Container>
+            <Row>
+                <Col className="text-center py-3">
+                    <p>Pro shop &copy; {year}</p>
+                </Col>
+            </Row>
+        </Container>
+    </footer>
+}
+export default Footer
